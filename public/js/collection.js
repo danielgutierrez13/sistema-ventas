7843d3b78058f1a6b5629f0a0f638b3ee578b62e
Carlos Chininin <carloschininin@gmail.com>
2022-03-03 13:40:47 -0500

    update symfony

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,31 +1,27 @@
 /**
  Admin script
  **/
-var Collection = function () {
+let Collection = function () {
     //* BEGIN:CORE HANDLERS *//
 
-    var createChild = function (child,child_class) {
+    let createChild = function (child,child_class) {
         // creamos el contenedor
         child = typeof child !== 'undefined' ? child : 'li';
-        var custom_child = '<' + child + ' class="' + child_class + ' list-group-item"></' + child + '>' ;
-
-        return custom_child;
+        return '<' + child + ' class="' + child_class + ' list-group-item"></' + child + '>';
     };
 
-    // Handle Entity
-    var handleEntity = function (collection, child) {
-        var $addLink = $('<a href="#" class="btn btn-light-success btn-sm tm-btn m-btn--icon m-btn--pill"><span><i class="fa fa-plus"></i><span>Agregar</span></span></a>');
-        var $newLink = $(child).append($addLink);
+    let handleEntity = function (collection, child) {
+        let $addLink = $('<a href="#" class="btn btn-light-success btn-sm"><span><i class="fa fa-plus"></i><span>Agregar</span></span></a>');
+        let $newLink = $(child).append($addLink);
         initDataForm(collection, $addLink, $newLink, child);
     };
 
-    var initDataForm = function(collection, $addLink, $newLink, child){
-        var $collectionHolder = $(collection);
+    let initDataForm = function(collection, $addLink, $newLink, child){
+        let $collectionHolder = $(collection);
         $collectionHolder.find('li').each(function() {
             addBlockFormDeleteLink($(this));
         });
-        $collectionHolder.append($newLink); //$collectionHolder.prepend($newLink);
-        //$collectionHolder.data('index', $collectionHolder.find(':input').length);
+        $collectionHolder.append($newLink);
         $collectionHolder.data('index', $collectionHolder.find('li').length);
         $addLink.on('click', function(e) {
             e.preventDefault();
@@ -33,45 +29,39 @@ var Collection = function () {
         });
     };
 
-    var addBlockForm = function($collectionHolder, $newLink, child) {
-        var prototype = $collectionHolder.data('prototype');
-        var index = $collectionHolder.data('index');
-        var newForm;
+    let addBlockForm = function($collectionHolder, $newLink, child) {
+        let prototype = $collectionHolder.data('prototype');
+        let index = $collectionHolder.data('index');
+        let newForm;
         if($collectionHolder.data('prototype-name')){
-            var prototype_name = new RegExp($collectionHolder.data('prototype-name'), 'gi');
+            let prototype_name = new RegExp($collectionHolder.data('prototype-name'), 'gi');
             newForm = prototype.replace(prototype_name, index);
         }
         else{
             newForm = prototype.replace(/__name__/g, index);
         }
         $collectionHolder.data('index', index + 1);
-        var $newFormLi = $(child).append(newForm);
-        $newLink.before($newFormLi); //$newLink.append($newFormLi);
+        let $newFormLi = $(child).append(newForm);
+        $newLink.before($newFormLi);
         addBlockFormDeleteLink($newFormLi);
 
         return $newFormLi;
     };
 
-    var addBlockFormDeleteLink = function($formLi) {
-        var $removeFormA = $('<a href="#" class="btn btn-sm btn-light-danger btn-icon"><i class="fa fa-times"></i></a>');
-        //var $auxForm = $('<div class="col-sm-1"></div>').append($removeFormA);
-        //$tagFormLi.children('.row').append($auxForm);
+    let addBlockFormDeleteLink = function($formLi) {
+        let $removeFormA = $('<a href="#" class="btn btn-sm btn-light-danger btn-icon"><i class="fa fa-times"></i></a>');
         $formLi.append($removeFormA);
         $removeFormA.on('click', function(e) {
-            // prevent the link from creating a "#" on the URL
             e.preventDefault();
-            // remove the li for the tag form
             $formLi.remove();
         });
     };
 
     //* END:CORE HANDLERS *//
-
     return {
         init: function (collection,child) {
-            var custom_child = createChild(child,'collection_child');
+            let custom_child = createChild(child,'collection_child');
             handleEntity(collection,custom_child);
         },
     };
-
 }();
